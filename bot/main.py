64198c2d47e2b7eb6358a7d855e206b7da489888
Saran.IT <saranit.service@gmail.com>
2024-05-16 16:15:56 +0700

    commit

--- bot/main.py
+++ bot/main.py
@@ -27,7 +27,7 @@ def main():
         exit(1)
 
     # Setup configurations
-    model = os.environ.get('OPENAI_MODEL', 'gpt-4.0')
+    model = os.environ.get('OPENAI_MODEL', 'gpt-4')
     functions_available = are_functions_available(model=model)
     max_tokens_default = default_max_tokens(model=model)
     openai_config = {
